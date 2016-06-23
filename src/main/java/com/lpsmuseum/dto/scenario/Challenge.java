f571c08073051ce2d8338fa32dc5265df4dec565
ktorz14 <filipefurquimfelix@gmail.com>
2016-06-23 16:57:53 -0300

    inicializacao parametros challenge

--- src/main/java/com/lpsmuseum/dto/scenario/Challenge.java
+++ src/main/java/com/lpsmuseum/dto/scenario/Challenge.java
@@ -31,6 +31,11 @@ public class Challenge {
 	 * Class constructor.
 	 */
 	public Challenge() {
+            
+            
+            description = "";
+            answers = new ArrayList<Answer>();
+            
 	}
 
 	/**
